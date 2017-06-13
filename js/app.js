f85557e40787c94e5e64ae546dcb2a744e1d6c50
Clauss Franco <you@example.com>
2017-06-12 20:00:46 -0500

    Primera Funcionabilidad

--- js/app.js
+++ js/app.js
@@ -1,15 +1,48 @@
-$.getJSON("http://pokeapi.co/api/v2/pokemon/", 
-	function (response) {
-	var pokemons = response.results;
-	console.log(pokemons);
-});
-
 var cargarPagina = function(){
+    cargarPersonajes();
     $('.modal').modal();
+
+    //    $(document).on("click", ".poderes", mostrarPoderes);
+};
+
+var cargarPersonajes = function(){
+    var url = 'http://pokeapi.co/api/v2/pokemon/';
+    $.getJSON(url, function(response){
+        var personajes = response.results;
+        mostrarPersonajes(personajes);
+    });  
+};
+var plantillaPokemon = '<div class="col s12 m2 pokemon">' +
+    '<div class="card">' +
+    '<div class="card-image">'+
+    '<img src="assets/img/**nombreFoto**.png">' +
+    '</div>'+
+    '<div class="card-title">'+
+    '<h4>**nombre**</h4>'+
+    '</div>'+
+    '<div class="card-action">'+
+    '<a href="#modal1" class="poderes">Ver Poderes</a>' +
+    '</div>'+
+    '</div>'+
+    '</div>'; 
+
+var mostrarPersonajes = function(personajes){
+    var $listaPokemon = $('#listaDePokemones');
+    var plantillaFinal=" ";
+
+    personajes.forEach(function(personaje,index){
+        plantillaFinal += plantillaPokemon.replace('**nombre**',personaje.name).replace('**nombreFoto**',personaje.name) 
+        
+    });
+    
+    $listaPokemon.html(plantillaFinal);
+
 };
 
 
 
 
- $(document).ready(cargarPagina);
- 
+
+
+
+$(document).ready(cargarPagina);
